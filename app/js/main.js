330221f25cb122630b69a28115b4013fb5c866bd
sophietk <sophie.tk@gmail.com>
2015-03-16 17:58:11 +0100

    feature(people): view parents/children tree

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,20 @@ app
         Marionette.Renderer.render = function (template, data) {
             return Handlebars.compile($(template).html())(data);
         };
+        Handlebars.registerPartial('gender-symbol', $('#partial-gender-symbol').html());
         Handlebars.registerPartial('people-link', $('#partial-people-link').html());
+        Handlebars.registerPartial('tree-node', $('#partial-tree-node').html());
+        Handlebars.registerHelper('year', function (date) {
+            if (_.isUndefined(date)) return '';
+            return moment(date).year();
+        });
+        Handlebars.registerHelper('oneOf', function (obj1, obj2, options) {
+            if (_.isUndefined(obj1) && _.isUndefined(obj2)) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        });
     })
     .on('start', function () {
         app.menuRegion.show(new MenuView());
