56680af067ca19b45cce98082744d3759d13bad5
sophietk <sophie.tk@gmail.com>
2015-03-17 20:18:52 +0100

    technical(server): json file modified at creation/update

--- server/database/index.js
+++ server/database/index.js
@@ -1,8 +1,17 @@
 var _ = require('lodash'),
-    all = require('./people.json');
+    all = require('./people.json'),
+    fs = require('fs'),
+    path = require('path');
 
 function getPeople(id) {
     return _.cloneDeep(_.findWhere(all, {_id: id}));
+}
+
+function updateFile() {
+    fs.writeFile(path.resolve(__dirname, 'people.json'), JSON.stringify(all), function (err) {
+        if (err) return console.log(err);
+        console.log('File updated');
+    });
 }
 
 exports = module.exports = {
@@ -22,16 +31,19 @@ exports = module.exports = {
     replacePeople: function (id, people) {
         var index = _.findIndex(all, {_id: id});
         all[index] = people;
+        updateFile();
     },
 
     deletePeople: function (id) {
         all = _.without(all, getPeople(id));
+        updateFile();
     },
 
     createPeople: function (people) {
         var id = _.uniqueId();
         people._id = id;
         all.push(people);
+        updateFile();
         return id;
     }
 };
