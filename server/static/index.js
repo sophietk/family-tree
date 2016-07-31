6d8fc4829413643697b64ea577138ca13f482149
sophietk <sophie.tk@gmail.com>
2016-07-31 02:31:52 +0200

    refactoring(favicon): serve favicon from index.html

--- server/static/index.js
+++ server/static/index.js
@@ -1,14 +1,13 @@
 var express = require('express'),
     path = require('path'),
-    favicon = require('serve-favicon'),
     DEV_DIR = '/../../app',
     DIST_DIR = '/../../dist';
 
 exports = module.exports = function (app, env) {
 
-    app.use(favicon(__dirname + DEV_DIR + '/img/favicon.ico'));
+    var staticDir = path.join(__dirname, env === 'prod' ? DIST_DIR : DEV_DIR);
 
     // Serve static files (dev or dist)
-    app.use(express.static(path.join(__dirname, env === 'prod' ? DIST_DIR : DEV_DIR)));
+    app.use(express.static(staticDir));
 
 };
