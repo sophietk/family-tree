1dfff6396b3252bbec7a54613d90b662ed2d81e4
sophietk <sophie.tk@gmail.com>
2019-01-26 01:01:23 +0100

    fix(es6): go back to 90's for usemin compatibility

--- app/js/family/stats/StatsView.js
+++ app/js/family/stats/StatsView.js
@@ -3,35 +3,35 @@ var StatsView = Marionette.View.extend({
 
   templateContext: function () {
     var allPeople = this.getAllPeopleInFamily(this.model.toJSON())
-    var maleCount = allPeople.filter(people => people.gender === 'M').length
-    var femaleCount = allPeople.filter(people => people.gender === 'F').length
-    var alivePeople = allPeople.filter(people => !people.deathDate)
+    var maleCount = allPeople.filter(function (people) { return people.gender === 'M' }).length
+    var femaleCount = allPeople.filter(function (people) { return people.gender === 'F' }).length
+    var alivePeople = allPeople.filter(function (people) { return !people.deathDate })
     var averageAge = this.getAverageAge(alivePeople)
 
     return {
-      maleCount,
-      femaleCount,
-      averageAge
+      maleCount: maleCount,
+      femaleCount: femaleCount,
+      averageAge: averageAge
     }
   },
 
   getAllPeopleInFamily: function (familyPeople) {
-    const spouses = familyPeople.spouses.filter(spouse => spouse._id !== undefined)
+    const spouses = familyPeople.spouses.filter(function (spouse) { return spouse._id !== undefined })
     const children = familyPeople.spouses
-      .flatMap(spouse => spouse.children || [])
-      .flatMap(child => this.getAllPeopleInFamily(child))
+      .flatMap(function (spouse) { return spouse.children || [] })
+      .flatMap(function (child) { return this.getAllPeopleInFamily(child) })
 
-    return [familyPeople, ...spouses, ...children]
+    return [familyPeople, spouses, children].flat()
   },
 
   getAverageAge: function (allPeople) {
-    var birthDays = allPeople.map(people => people.birthDate)
-      .filter(birthDate => !!birthDate)
-      .map(birthDate => moment().diff(moment(birthDate), 'days'))
+    var birthDays = allPeople.map(function (people) { return people.birthDate })
+      .filter(function (birthDate) { return !!birthDate })
+      .map(function (birthDate) { return moment().diff(moment(birthDate), 'days') })
 
     if (birthDays.length === 0) return
 
-    var averageBirthDays = birthDays.reduce((sum, value) => sum + value) / birthDays.length
+    var averageBirthDays = birthDays.reduce(function (sum, value) { return sum + value }) / birthDays.length
     var averageBirthDate = moment().add(-averageBirthDays, 'days')
     return averageBirthDate.fromNow(true)
   }
